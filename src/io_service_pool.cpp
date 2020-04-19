dd3649a32ed4656d374bd6abb3007e9ca94f7e65
Jacky Lee <JackyLeeHongJian@Gmail.com>
2020-04-19 19:47:16 +0800

    Renamed io_service_pool constructor argument from concurrency to num_threads.

--- src/io_service_pool.cpp
+++ src/io_service_pool.cpp
@@ -1,10 +1,10 @@
 #include "io_service_pool.hpp"
 
-io_service_pool::io_service_pool(const size_t concurrency) :
+io_service_pool::io_service_pool(const size_t num_threads) :
 	w(make_unique<work>(*this))
 {
-	reserve(concurrency);
-	for (size_t i = 0; i < concurrency; ++i)
+	reserve(num_threads);
+	for (size_t i = 0; i < num_threads; ++i)
 	{
 		emplace_back(async(launch::async, [&]()
 		{
