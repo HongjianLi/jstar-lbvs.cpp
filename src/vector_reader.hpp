0a50ba590d0de14b8da6b7133d73b6eb1254668f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2020-04-24 19:21:49 +0800

    Rewrote header_vector and stream_vector classes to be inline function read_string()

--- src/vector_reader.hpp
+++ src/vector_reader.hpp
@@ -15,76 +15,38 @@ inline static auto local_time()
 }
 
 template <typename T>
-inline vector<T> read(const path src) // Sequential read can be very fast when using SSD
+inline void read_types(const path src, vector<T>& vec) // Sequential read can be very fast when using SSD.
 {
 	ifstream ifs(src, ios::binary | ios::ate);
 	const size_t num_bytes = ifs.tellg();
 	cout << local_time() << "Reading " << src.filename() << " of " << num_bytes << " bytes" << endl;
-	vector<T> buf;
-	buf.resize(num_bytes / sizeof(T));
+	vec.resize(num_bytes / sizeof(T));
 	ifs.seekg(0);
-	ifs.read(reinterpret_cast<char*>(buf.data()), num_bytes);
-	return buf;
+	ifs.read(reinterpret_cast<char*>(vec.data()), num_bytes);
 }
 
-inline vector<string> readLines(const path src) // Sequential read can be very fast when using SSD
+inline void read_lines(const path src, vector<string>& vec) // Sequential read can be very fast when using SSD.
 {
 	ifstream ifs(src, ios::binary | ios::ate);
 	const size_t num_bytes = ifs.tellg();
 	cout << local_time() << "Reading " << src.filename() << " of " << num_bytes << " bytes" << endl;
-	vector<string> vec;
-	vec.reserve(num_bytes / 6); // An average line size is 6 bytes.
+	vec.reserve(num_bytes / 17); // Assuming an average line size is 17 bytes (e.g. a ZINC ID line has 16 characters plus \n).
 	ifs.seekg(0);
 	string line;
 	while (getline(ifs, line)) {
 		vec.push_back(move(line));
 	}
-	return vec;
 }
 
-//! Represents a vector of headers, which are the positions of the first character of each entity in a vector file.
-template <typename size_type> // Size type of the encoded binary representation of the footer file. This is usually size_t in order to support seeking vector files of >4GB.
-class header_vector
+inline string read_string(const vector<size_t>& ftr, const size_t index, ifstream& ifs)
 {
-public:
-	//! Reads the footer file for the vector file.
-	explicit header_vector(path src) :
-		ftr(read<size_t>(src.replace_extension(src.extension().string() + ".ftr")))
-	{
-	}
-
-	size_t size() const
-	{
-		return ftr.size();
-	}
-
-protected:
-	vector<size_type> ftr;
-};
-
-template <typename size_type>
-class stream_vector : public header_vector<size_type>
-{
-public:
-	explicit stream_vector(const path src) :
-		header_vector<size_type>(src),
-		ifs(src, ios::binary)
-	{
-	}
-
-	string operator[](const size_t index)
-	{
-		const auto pos = index ? this->ftr[index - 1] : 0;
-		const auto len = this->ftr[index] - pos;
-		string buf;
-		buf.resize(len);
-		ifs.seekg(pos);
-		ifs.read(const_cast<char*>(buf.data()), len);
-		return buf;
-	}
-
-protected:
-	ifstream ifs;
-};
+	const auto pos = index ? ftr[index - 1] : 0;
+	const auto len = ftr[index] - pos;
+	string str;
+	str.resize(len);
+	ifs.seekg(pos);
+	ifs.read(const_cast<char*>(str.data()), len);
+	return str;
+}
 
 #endif
